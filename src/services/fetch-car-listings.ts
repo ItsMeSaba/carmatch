f12adcbf17e8fabafdd80355980b1f61b7daf39d
Saba Silagadze <sbsilagadze@gmail.com>
2025-12-13 19:47:13 +0400

    feat: fetches and logs data to test if api is accessible

--- src/services/fetch-car-listings.ts
+++ src/services/fetch-car-listings.ts
@@ -2,11 +2,14 @@
 
 import { dummyContent } from "../shared/data/dummy-content";
 
-// const API_ENDPOINT =
-//   "https://api2.myauto.ge/ka/products?TypeID=0&ForRent=0&Mans=&CurrencyID=3";
+const API_ENDPOINT =
+  "https://api2.myauto.ge/ka/products?TypeID=0&ForRent=0&Mans=&CurrencyID=3";
 
 export async function fetchCarListings() {
-  // const res = await fetch(API_ENDPOINT);
+  const res = await fetch(API_ENDPOINT);
+  const data = await res.json();
+
+  console.log("data", data);
 
   return dummyContent.data.items;
 }
